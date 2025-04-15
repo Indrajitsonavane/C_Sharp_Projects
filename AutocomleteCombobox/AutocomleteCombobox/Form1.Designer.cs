249876b1c8c21156580b786d3feb5f995c40d2e1
indrajitsonavane <indrajitsonavane1@gmail.com>
2025-04-15 10:31:35 -0700

    Auto Complete Text

---
+++ AutocomleteCombobox/AutocomleteCombobox/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace AutocomleteCombobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmb_P_Name = new System.Windows.Forms.ComboBox();
+            this.lbl_Product_Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmb_P_Name
+            // 
+            this.cmb_P_Name.DropDownHeight = 200;
+            this.cmb_P_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_P_Name.FormattingEnabled = true;
+            this.cmb_P_Name.IntegralHeight = false;
+            this.cmb_P_Name.ItemHeight = 38;
+            this.cmb_P_Name.Location = new System.Drawing.Point(256, 48);
+            this.cmb_P_Name.Name = "cmb_P_Name";
+            this.cmb_P_Name.Size = new System.Drawing.Size(536, 46);
+            this.cmb_P_Name.TabIndex = 0;
+            // 
+            // lbl_Product_Name
+            // 
+            this.lbl_Product_Name.AutoSize = true;
+            this.lbl_Product_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Product_Name.Location = new System.Drawing.Point(35, 56);
+            this.lbl_Product_Name.Name = "lbl_Product_Name";
+            this.lbl_Product_Name.Size = new System.Drawing.Size(175, 29);
+            this.lbl_Product_Name.TabIndex = 1;
+            this.lbl_Product_Name.Text = "Product Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 499);
+            this.Controls.Add(this.lbl_Product_Name);
+            this.Controls.Add(this.cmb_P_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb_P_Name;
+        private System.Windows.Forms.Label lbl_Product_Name;
+    }
+}
+
---
+++ AutocomleteCombobox/AutocomleteCombobox/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace AutocomleteCombobox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Auto();
+
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=Temp;Integrated Security=True");
+
+            con.Open();
+
+            SqlCommand Cmd = new SqlCommand("Select Product_Name , barcode From Product_Details", con);
+
+            SqlDataReader Dr = Cmd.ExecuteReader();
+
+            while (Dr.Read())
+            {
+
+               
+                cmb_P_Name.Items.Add(Dr["barcode"].ToString());
+
+            }
+
+
+
+
+        }
+
+        void Auto()
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=Temp;Integrated Security=True");
+
+            con.Open();
+
+            SqlCommand Cmd = new SqlCommand("Select Product_Name , barcode From Product_Details", con);
+
+            SqlDataReader Dr = Cmd.ExecuteReader();
+
+            AutoCompleteStringCollection Autocom = new AutoCompleteStringCollection();
+
+            
+
+            while (Dr.Read())
+            {
+
+                Autocom.Add(Dr["Product_Name"].ToString());
+                Autocom.Add(Dr["barcode"].ToString());
+
+            }
+
+
+
+            this.cmb_P_Name.AutoCompleteMode = AutoCompleteMode.Suggest;
+            cmb_P_Name.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            cmb_P_Name.AutoCompleteCustomSource = Autocom;
+
+        }
+    }
+}
