8e1883a35d2f5e14ed245c572f6004a939add1f2
indrajitsonavane <indrajitsonavane1@gmail.com>
2025-04-24 10:39:09 -0700

    Excel Data Add

---
+++ Insert_Data_rom_Excel_To_Datagridview/Insert_Data_rom_Excel_To_Datagridview/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Insert_Data_rom_Excel_To_Datagridview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Select_Exel_File = new System.Windows.Forms.Button();
+            this.dgv_Data = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Select_Exel_File
+            // 
+            this.btn_Select_Exel_File.Location = new System.Drawing.Point(57, 49);
+            this.btn_Select_Exel_File.Name = "btn_Select_Exel_File";
+            this.btn_Select_Exel_File.Size = new System.Drawing.Size(164, 37);
+            this.btn_Select_Exel_File.TabIndex = 0;
+            this.btn_Select_Exel_File.Text = "Select Exel File";
+            this.btn_Select_Exel_File.UseVisualStyleBackColor = true;
+            this.btn_Select_Exel_File.Click += new System.EventHandler(this.btn_Select_Exel_File_Click);
+            // 
+            // dgv_Data
+            // 
+            this.dgv_Data.AllowUserToAddRows = false;
+            this.dgv_Data.AllowUserToDeleteRows = false;
+            this.dgv_Data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Data.Location = new System.Drawing.Point(12, 130);
+            this.dgv_Data.Name = "dgv_Data";
+            this.dgv_Data.ReadOnly = true;
+            this.dgv_Data.RowHeadersWidth = 51;
+            this.dgv_Data.RowTemplate.Height = 24;
+            this.dgv_Data.Size = new System.Drawing.Size(1269, 502);
+            this.dgv_Data.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1293, 661);
+            this.Controls.Add(this.dgv_Data);
+            this.Controls.Add(this.btn_Select_Exel_File);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Data)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Select_Exel_File;
+        private System.Windows.Forms.DataGridView dgv_Data;
+    }
+}
+
---
+++ Insert_Data_rom_Excel_To_Datagridview/Insert_Data_rom_Excel_To_Datagridview/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insert_Data_rom_Excel_To_Datagridview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void btn_Select_Exel_File_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "Excel Sheet(*.Xlsx)|*.Xlsx|All Files(*.*)|*.*";
+            if (opd.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = opd.FileName;
+
+                string con = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = {0};Extended Properties = 'Excel 8.0;HDR={1}'";
+
+                con = string.Format(con , filepath , "yes");
+
+                OleDbConnection exelcon = new OleDbConnection(con);
+                exelcon.Open();
+
+                DataTable dtexcel = exelcon.GetOleDbSchemaTable(OleDbSchemaGuid.Tables ,null);
+
+                string exelsheet = dtexcel.Rows[0]["TABLE_NAME"].ToString();
+
+                OleDbCommand cmd = new OleDbCommand("Select * from["+ exelsheet +"]",exelcon);
+
+                OleDbDataAdapter oda = new OleDbDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                oda.Fill(dt);
+
+                dgv_Data.DataSource = dt;
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
